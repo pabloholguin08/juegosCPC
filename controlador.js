b7523cd81b35a68f56fd4b78ef18bb370651a059
pabloholguin08 <jpholguin06@gmail.com>
2021-09-15 16:31:08 -0500

    aplicacion 15/09/2021

--- controlador.js
+++ controlador.js
@@ -1,18 +1,35 @@
-let calcular = document.getElementById("calcular");
-calcular.addEventListener("click",verificar);
+let calcular=document.getElementById("calcular");
+calcular.addEventListener("click",verificarRespuesta);
+
+let numeroA=document.getElementById("siguiente");
+numeroA=addEventListener("click",aleatorio)
+
 let gif=document.getElementById("gif");
 
-function verificar(){
+function verificarRespuesta(){
+
     let numero1=document.getElementById("numero1").value;
     let numero2=document.getElementById("numero2").value;
     let resultado=document.getElementById("resultado").value;
 
     let calcularResultado=Number(numero1)+Number(numero2);
+    console.log("hice click");
 
     if(resultado==calcularResultado){
         gif.classList.remove("invisible");
-        gif.classList.add("visible");
+        gif.src="https://giphy.com/embed/l0MYKDrj6SXHz8YYU"
     }else{
-
+        gif.classList.remove("invisible");
+        gif.src="https://giphy.com/embed/3jmqIaBE8x86xwbSbD"
     }
 }
+
+function numeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function aleatorio(){
+    numero1.value=numeroAleatorio(1,10);
+    numero2.value=numeroAleatorio(1,10);
+}
+
